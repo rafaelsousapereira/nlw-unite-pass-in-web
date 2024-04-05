8ebaf28d12e753aafa3d9bae6a9e95fdad12e8e4
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-05 13:55:39 -0300

    fix: remove comment

--- pass-in-web/src/components/icon-button.tsx
+++ pass-in-web/src/components/icon-button.tsx
@@ -8,10 +8,6 @@ interface IconButtonProps extends ComponentProps<"button"> {
 export const IconButton = ({ transparent, ...props }: IconButtonProps) => {
   return (
     <button {...props} 
-      // className={transparent 
-      //   ? "border border-white/10 rounded bg-black/20 p-1.5" 
-      //   : "border border-white/10 rounded bg-white/10 p-1.5"} 
-      // />
       className={twMerge(
         "border border-white/10 rounded-md p-1.5",
         transparent ? "bg-black/20" : "bg-white/10",
